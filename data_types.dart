7f72caf139e3d0cac6ce873d6d5f35693671d50f
Steve-austine <steveaustine001@gmail.com>
2024-03-07 17:34:19 +0300

    first commit

---
+++ data_types.dart
@@ -0,0 +1,17 @@
+
+void main(){
+  //numbers
+  int num1 = 56;
+  double num2 = 10.5;
+
+  // strings
+  String name = "John Doe";
+
+  //lists
+  List shopping = ["sugar", "bathing soap", "salt"];
+
+  //maps
+  Map FoodPrices = {"rice" : 50, "beans" : 30, "meat" : 100};
+
+
+}
